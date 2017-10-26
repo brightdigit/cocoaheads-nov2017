2ea02470c6b38ecbe7e5c9d95723f2be3e22fed8
Leo Dion <leogdion@brightdigit.com>
2017-10-26 13:31:14 -0400

    Initial Commit

---
+++ my-first-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my-first-app
+//
+//  Created by Leo Dion on 10/26/17.
+//  Copyright © 2017 Bright Digit, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
