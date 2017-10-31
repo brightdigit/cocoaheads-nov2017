b18bc1da7a0a9e6039130695b76cfd494a2f2fce
Leo Dion <leogdion@brightdigit.com>
2017-10-31 14:06:00 -0400

    adding ViewController

--- my-first-app/ViewController.swift
+++ my-first-app/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  @IBOutlet var label: UILabel!
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    self.label.text = "Hello World!"
   }
 
   override func didReceiveMemoryWarning() {
